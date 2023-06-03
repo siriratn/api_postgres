ea407db69966142ec1a086a5cd0c9a950f1da1e3
sirirat <srirat@example.com>
2023-06-04 01:06:24 +0700

    The handlers.CreateFact

---
+++ handlers/facts.go
@@ -0,0 +1,28 @@
+package handlers
+
+
+import (
+    "github.com/gofiber/fiber/v2"
+	"github.com/siriratn/api_postgres/database"
+	"github.com/siriratn/api_postgres/models"
+
+
+)
+
+
+func Home(c *fiber.Ctx) error {
+    return c.SendString("Helllo Nidnoi:)")
+}
+
+func CreateFact(c *fiber.Ctx) error {
+    fact := new(models.Fact)
+    if err := c.BodyParser(fact); err != nil {
+        return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+            "message": err.Error(),
+        })
+    }
+
+    database.DB.Db.Create(&fact)
+
+    return c.Status(200).JSON(fact)
+}
