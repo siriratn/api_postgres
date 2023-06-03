fd915ead881974a51933e3edfb133ba74e2e259d
sirirat <srirat@example.com>
2023-06-04 00:43:30 +0700

    move our existing handler into handlers/facts.go

--- cmd/routes.go
+++ cmd/routes.go
@@ -5,10 +5,15 @@ package main
 
 import (
     "github.com/gofiber/fiber/v2"
+	"github.com/siriratn/api_postgres/handlers"
 )
 
 func setupRoutes(app *fiber.App) {
-	
+    app.Get("/", handlers.Home)
+}
+
+func setupRoutes(app *fiber.App) {
+
     app.Get("/", func(c *fiber.Ctx) error {
         return c.SendString("Hello, Nidnoi!")
     })
