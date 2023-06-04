d395a6c3f6739b7fa86b819e8af6ce47d9d7971b
sirirat <srirat@example.com>
2023-06-04 20:01:09 +0700

    Send PATCH request with JavaScript Done

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -14,3 +14,28 @@
         } );
     }
 })()
+
+
+// Get the editForm element and the current fact ID to edit
+const editForm = document.querySelector('#form-update-fact')
+const factToEdit = editForm && editForm.dataset.factid
+
+// Add an event listener to listen for the form submit
+editForm && editForm.addEventListener('submit', (event) => {
+    // Prevent the default behaviour of the form element
+    event.preventDefault()
+
+    // Convert form data into a JavaScript object
+    const formData = Object.fromEntries(new FormData(editForm));
+
+    return fetch(`/fact/${factToEdit}`, {
+        // Use the PATCH method
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        // Convert the form's Object data into JSON
+        body: JSON.stringify(formData),
+    })
+    .then(() => document.location.href=`/fact/${factToEdit}`)// Redirect to show
+})
